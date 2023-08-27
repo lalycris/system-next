5f6d1825023b2ba85a0c1dcfe68cc63094206ab5
lalycris <larissaoliveira.dev@gmail.com>
2023-08-27 17:44:41 -0400

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+
+const Home: React.FC = () => (
+    <div>
+        <Head>
+            <title>Starter Next</title>
+        </Head>
+
+        <main>
+            <h1>Página Inicial</h1>
+        </main>
+    </div>
+);
+
+export default Home;

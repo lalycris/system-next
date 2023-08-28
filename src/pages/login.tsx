5125cd194380a847ed1b81ca1c07fc88975423dc
lalycris <larissaoliveira.dev@gmail.com>
2023-08-27 20:37:33 -0400

    initial commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './login.css';
+import Footer from '../app/components/Footer/Footer';
+
+function Login() {
+    return (
+        <><Footer /><><div className='h1'>
+            <h1>Login</h1>
+        </div>
+            <button> Clicar </button>
+        </></>
+
+    );
+}
+
+export default Login;

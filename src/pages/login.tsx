4f886e4df29a58971a6003cedbc96cc616511378
lalycris <larissaoliveira.dev@gmail.com>
2023-09-12 17:42:30 -0400

    feat login screen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,42 @@
 import React from 'react';
 import './login.css';
-import Footer from '../app/components/Footer/Footer';
+import { ClassNames } from '@emotion/react';
+import Link from 'next/link';
+
 
 function Login() {
     return (
-        <><Footer /><><div className='h1'>
-            <h1>Login</h1>
-        </div>
-            <button> Clicar </button>
-        </></>
+    <div className="login">
+        <div className="login-card">
+            <img style={{
+                width: '90px', 
+                height: '80px', 
+                marginLeft: 'auto', 
+                marginRight: 'auto', 
+                display: 'block'}}
 
-    );
+             src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-download-logo-30.png" alt="Spotify" />
+            
+            <h2>Login</h2>
+            
+                <div>
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" name="email" id="email" placeholder="Email" />
+                </div>
+                
+                <div>
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" name="password" id="password" placeholder="Senha" />
+                   
+                    <Link href="/forgot-password">Esqueceu a senha?</Link>
+                </div>
+
+                <button type="submit">Entrar</button>
+                <button type="submit">Cadastrar-se</button>
+            
+        </div>
+    </div>
+        )
 }
 
 export default Login;
